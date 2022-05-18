3c6a6689f957d1148dc9cc2ea2568217b8f96cdd
Rogerio Barreto <rabarreto26@hotmail.com>
2022-05-18 12:31:31 -0300

    Exercicio Polimorfismo

---
+++ ImportedProduct.java
@@ -0,0 +1,34 @@
+package exercicio2polimorfismo;
+
+public class ImportedProduct extends Product{
+	 private Double customsFee;
+	 
+	 public ImportedProduct() {		 
+	 }
+
+	public ImportedProduct(String name, double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public Double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(Double customsFee) {
+		this.customsFee = customsFee;
+	}
+	public Double totalPrice() {
+		return getPrice() + customsFee;
+	}
+	 @Override
+	 public String priceTag() {
+		 return getName() 
+					+ " $ " 
+					+ String.format("%.2f", totalPrice())
+					+ " (Customs fee: $ " 
+					+ String.format("%.2f", customsFee)
+					+ ")";
+		}
+
+}
---
+++ package-info.java
@@ -0,0 +1,1 @@
+package exercicio2polimorfismo;
